51ac97a108effe0f5252a88a1a96a21fe509ad26
JAREK <jarek.styba@gmail.com>
2018-04-22 13:58:21 +0200

    first commit

---
+++ app/view2/view2.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('myApp.view2', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view2', {
+    templateUrl: 'view2/view2.html',
+    controller: 'View2Ctrl'
+  });
+}])
+
+.controller('View2Ctrl', ['$http', function($http) {
+    this.formUser = {
+        'login': '',
+        'password': '',
+        'email': ''
+    };
+    this.sendToBackend = function () {
+        $http.post(URL + "/user/register", self.formUser)
+            .then(function (data) {
+                console.log(data);
+            }, function (data) {
+
+            });
+    };
+}]);
